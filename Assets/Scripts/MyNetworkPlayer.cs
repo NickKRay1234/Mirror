cd64d265bc337f009bd289911518963be2ebc96f
Mykola Kalchuk <80714127+NickKRay1234@users.noreply.github.com>
2023-05-06 19:37:27 +0200

    SyncVar Callbacks;

--- Assets/Scripts/MyNetworkPlayer.cs
+++ Assets/Scripts/MyNetworkPlayer.cs
@@ -1,15 +1,18 @@
 using Mirror;
+using TMPro;
 using UnityEngine;
 
 public class MyNetworkPlayer : NetworkBehaviour
 {
-    [SyncVar] [SerializeField] private string _displayName = "Missing Name";
-    [SyncVar] [SerializeField] private Color _displayColor;
+    [SerializeField] private TMP_Text _displayNameText = null;
+    [SerializeField] private Renderer _displayColourRenderer = null;
+    [SyncVar(hook = nameof(HandleDisplayNameUpdated))] [SerializeField] private string _displayName = "Missing Name";
+    [SyncVar(hook = nameof(HandleDisplayColorUpdated))] [SerializeField] private Color _displayColor;
 
-    [Server]
-    public void SetDisplayName(string newDisplayName) =>
-        _displayName = newDisplayName;
+    [Server] public void SetDisplayName(string newDisplayName) => _displayName = newDisplayName;
+    [Server] public void SetColor(Color color) => _displayColor = color;
 
-    [Server]
-    public void SetColor(Color color) => _displayColor = color;
+    private void HandleDisplayColorUpdated(Color oldColor, Color newColor) =>
+        _displayColourRenderer.material.color = newColor;
+    private void HandleDisplayNameUpdated(string oldName, string newName) => _displayNameText.SetText(newName);
 }
