2565899370d02bc3c8decda255f6d7d68dbdde28
Mykola Kalchuk <80714127+NickKRay1234@users.noreply.github.com>
2023-05-07 21:43:31 +0200

    Remote Action;

--- Assets/Scripts/MyNetworkPlayer.cs
+++ Assets/Scripts/MyNetworkPlayer.cs
@@ -9,10 +9,33 @@ public class MyNetworkPlayer : NetworkBehaviour
     [SyncVar(hook = nameof(HandleDisplayNameUpdated))] [SerializeField] private string _displayName = "Missing Name";
     [SyncVar(hook = nameof(HandleDisplayColorUpdated))] [SerializeField] private Color _displayColor;
 
+    [Command]
+    private void CmdSetDisplayName(string newDisplayName)
+    {
+        RpcLogNewName(newDisplayName);
+        SetDisplayName(newDisplayName);
+    }
+
+    [ContextMenu("Set My Name")]
+    public void SetMyName()
+    {
+        CmdSetDisplayName("My New Name");
+    }
+
+    [ClientRpc]
+    private void RpcLogNewName(string newDisplayName)
+    {
+        Debug.Log(newDisplayName);
+    }
+
+
+    #region Server
     [Server] public void SetDisplayName(string newDisplayName) => _displayName = newDisplayName;
     [Server] public void SetColor(Color color) => _displayColor = color;
-
+    #endregion
+    #region Client
     private void HandleDisplayColorUpdated(Color oldColor, Color newColor) =>
         _displayColourRenderer.material.color = newColor;
     private void HandleDisplayNameUpdated(string oldName, string newName) => _displayNameText.SetText(newName);
+    #endregion
 }
