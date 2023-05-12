3ce885de78cf31b6787dc8493c9d5459fd8b9b3c
Mykola Kalchuk <80714127+NickKRay1234@users.noreply.github.com>
2023-05-12 16:41:24 +0200

    Added Spawning Units;

--- Assets/Scripts/Networking/RTSNetworking.cs
+++ Assets/Scripts/Networking/RTSNetworking.cs
@@ -1,9 +1,16 @@
 using Mirror;
+using UnityEngine;
 
 namespace Networking
 {
     public class RTSNetworking : NetworkManager
     {
-
+        [SerializeField] private GameObject _unitSpawnerPrefab = null;
+        public override void OnServerAddPlayer(NetworkConnectionToClient conn)
+        {
+            base.OnServerAddPlayer(conn);
+            GameObject unitSpawnerInstance = Instantiate(_unitSpawnerPrefab, conn.identity.transform.position, conn.identity.transform.rotation);
+            NetworkServer.Spawn(unitSpawnerInstance, conn);
+        }
     }
 }
