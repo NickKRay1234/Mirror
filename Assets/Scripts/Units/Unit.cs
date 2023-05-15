0423dee724c4d4fd7057e6184a77fb6dbea4adc7
Mykola Kalchuk <80714127+NickKRay1234@users.noreply.github.com>
2023-05-15 22:23:10 +0200

    Storing Units;

--- Assets/Scripts/Units/Unit.cs
+++ Assets/Scripts/Units/Unit.cs
@@ -1,3 +1,4 @@
+using System;
 using Mirror;
 using Mirror.Examples.Benchmark.Scripts;
 using UnityEngine;
@@ -10,11 +11,46 @@ namespace Units
         [SerializeField] private UnitMovement _unitMovement = null;
         [SerializeField] private UnityEvent onSelected = null;
         [SerializeField] private UnityEvent onDeselected = null;
+
+        public static event Action<Unit> ServerOnUnitSpawned; 
+        public static event Action<Unit> ServerOnUnitDespawned;
         
+        public static event Action<Unit> AuthorityServerOnUnitSpawned; 
+        public static event Action<Unit> AuthorityServerOnUnitDespawned;
+
+        #region Server
+
+        public override void OnStartServer()
+        {
+           ServerOnUnitSpawned?.Invoke(this);
+        }
+
+        public override void OnStopServer()
+        {
+            ServerOnUnitDespawned?.Invoke(this);
+        }
+
+        #endregion
+
         public UnitMovement GetUnitMovement() => _unitMovement;
 
         #region Client
 
+        public override void OnStartClient()
+        {
+            if (!isClientOnly || !hasAuthority) return;
+            AuthorityServerOnUnitSpawned?.Invoke(this);
+        }
+
+        public override void OnStopClient()
+        {
+            if (!isClientOnly || !hasAuthority) return;
+            AuthorityServerOnUnitDespawned?.Invoke(this);
+        }
+        
+        
+        
+        
         [Client]
         public void Select()
         {
