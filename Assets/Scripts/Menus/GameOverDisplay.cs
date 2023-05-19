0a3223c4f4b1e782a6ae68d344e956b14a26c46f
Mykola Kalchuk <80714127+NickKRay1234@users.noreply.github.com>
2023-05-19 15:31:37 +0200

    Winning The Game;

---
+++ Assets/Scripts/Menus/GameOverDisplay.cs
@@ -0,0 +1,40 @@
+using Buildings;
+using Mirror;
+using TMPro;
+using UnityEngine;
+
+namespace Menus
+{
+    public class GameOverDisplay : MonoBehaviour
+    {
+        [SerializeField] private GameObject _gameOverDisplayParent = null;
+        [SerializeField] private TMP_Text _winnerNameText = null;
+        private void Start()
+        {
+            GameOverHandler.ClientOnGameOver += ClientHandlerGameOver;
+        }
+
+        private void OnDestroy()
+        {
+            GameOverHandler.ClientOnGameOver -= ClientHandlerGameOver;
+        }
+
+        public void LeaveGame()
+        {
+            if (NetworkServer.active && NetworkClient.isConnected)
+            {
+                NetworkManager.singleton.StopHost();
+            }
+            else
+            {
+                NetworkManager.singleton.StopClient();
+            }
+        }
+        
+        private void ClientHandlerGameOver(string winner)
+        {
+            _winnerNameText.text = $"{winner} Has Won!";
+            _gameOverDisplayParent.SetActive(true);
+        }
+    }
+}
