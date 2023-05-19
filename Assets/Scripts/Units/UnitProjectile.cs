72fd26640109f32872acfaf92754392d39ecddd5
Mykola Kalchuk <80714127+NickKRay1234@users.noreply.github.com>
2023-05-19 11:44:28 +0200

    Dealing Damage;

--- Assets/Scripts/Units/UnitProjectile.cs
+++ Assets/Scripts/Units/UnitProjectile.cs
@@ -1,3 +1,5 @@
+using System;
+using Combat;
 using Mirror;
 using UnityEngine;
 
@@ -6,6 +8,7 @@ namespace Units
     public class UnitProjectile : NetworkBehaviour
     {
         [SerializeField] private Rigidbody rb = null;
+        [SerializeField] private int _damageToDeal = 20;
         [SerializeField] private float _destroyAfterSeconds = 5f;
         [SerializeField] private float _launchForce = 10f;
 
@@ -19,8 +22,20 @@ namespace Units
         {
             Invoke(nameof(DestroySelf), _destroyAfterSeconds);
         }
-        
-        
+
+        [ServerCallback]
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.TryGetComponent(out NetworkIdentity networkIdentity))
+            {
+                if (networkIdentity.connectionToClient == connectionToClient) return;
+            }
+            
+            if(other.TryGetComponent(out Health health)) health.DealDamage(_damageToDeal);
+            DestroySelf();
+        }
+
+
         [Server]
         private void DestroySelf()
         {
